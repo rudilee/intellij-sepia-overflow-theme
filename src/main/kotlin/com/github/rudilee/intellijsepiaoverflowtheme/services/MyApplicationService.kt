6f6ee0d89dbf2688ba370bacc8dba8e62d6c947f
GitHub Action <action@github.com>
2022-06-19 04:00:21 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/rudilee/intellijsepiaoverflowtheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rudilee.intellijsepiaoverflowtheme.services
+
+import com.github.rudilee.intellijsepiaoverflowtheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
